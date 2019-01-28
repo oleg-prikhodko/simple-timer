8185e4bb34022b900470da3ff85893d7de30dbdc
Oleg <super.oleg.3000@yandex.ru>
2019-01-28 18:24:00 +0300

    Add ability to stop timer

--- renderer.js
+++ renderer.js
@@ -4,7 +4,7 @@ durationInput.oninput = event => {
   event.target.value = event.target.value.replace(/\D/g, "");
 };
 
-function setElaplsed(elapsedMillis) {
+function setElapsed(elapsedMillis) {
   const elapsedSeconds = Math.round(elapsedMillis / 1000);
   let minutes = Math.floor(elapsedSeconds / 60);
   let seconds = elapsedSeconds % 60;
@@ -18,6 +18,7 @@ class Timer {
     this.duration = durationInSecs * 1000;
     this.elapsed = 0;
     this.ticking = false;
+    this.stop = this.stop.bind(this);
   }
 
   setDuration(durationInSecs) {
@@ -28,24 +29,34 @@ class Timer {
     this.ticking = true;
     this.elapsed = 0;
     this.startTime = Date.now();
-    const timeoutId = setInterval(() => {
+    this.timeoutId = setInterval(() => {
       this.elapsed = Date.now() - this.startTime;
       if (this.elapsed >= this.duration) {
-        this.ticking = false;
-        clearInterval(timeoutId);
-        durationInput.disabled = false;
-        setElaplsed(this.duration);
+        this.stop();
+        setElapsed(this.duration);
       } else {
-        setElaplsed(this.elapsed);
+        setElapsed(this.elapsed);
       }
     }, 1000);
+  }
+
+  stop() {
+    this.ticking = false;
+    clearInterval(this.timeoutId);
+    durationInput.disabled = false;
   }
 }
 const timer = new Timer();
 
 const button = document.getElementById("start-button");
 button.onclick = () => {
-  timer.setDuration(durationInput.value);
-  timer.start();
-  durationInput.disabled = true;
+  if (timer.ticking) {
+    timer.stop();
+    button.innerHTML = "Start"
+  } else {
+    timer.setDuration(durationInput.value);
+    timer.start();
+    durationInput.disabled = true;
+    button.innerHTML = "Stop"
+  }
 };
